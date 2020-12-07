1045562e68d858eaa5d6f1ffb2d7a326ff745aa9
Yaroslav Penkov <penyarik48@gmail.com>
2020-12-07 12:55:51 +0200

    Factory Pattern xample

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+abstract class TagsFactory{
+    abstract function getTag();
+
+    public function buildTag()
+    {
+        $tag = $this->getTag();
+        echo  $tag->create();
+
+    }
+}
+
+interface Tag{
+    public function create();
+
+}
+
+class pTag extends TagsFactory implements Tag{
+
+    private $content;
+
+    public function __construct($content){
+        $this->content = $content;
+    }
+
+    public function getTag()
+    {
+        return new pTag($this->content);
+    }
+    public function create()
+    {
+        return "<p> $this->content </p>";
+    }
+
+}
+
+class h1Tag extends TagsFactory implements Tag{
+
+    private $content;
+
+    public function __construct($content){
+        $this->content = $content;
+    }
+
+    public function getTag()
+    {
+        return new pTag($this->content);
+    }
+    public function create()
+    {
+        return "<h1>$this->content</h1>";
+    }
+
+}
+
